ef0f405d8508cf69a886b0e7ceaef55e730fab98
nyattoh <nyatto@gmail.com>
2025-04-07 06:10:52 +0900

    node_modulesとclaude_desktop_config.jsonを除外

--- node_modules/@mastra/core/dist/chunk-RG66XEJT.js
+++
@@ -1,8 +0,0 @@
-// src/storage/constants.ts
-var TABLE_WORKFLOW_SNAPSHOT = "mastra_workflow_snapshot";
-var TABLE_EVALS = "mastra_evals";
-var TABLE_MESSAGES = "mastra_messages";
-var TABLE_THREADS = "mastra_threads";
-var TABLE_TRACES = "mastra_traces";
-
-export { TABLE_EVALS, TABLE_MESSAGES, TABLE_THREADS, TABLE_TRACES, TABLE_WORKFLOW_SNAPSHOT };
--- node_modules/@mastra/mcp/dist/index.d.cts
+++
@@ -1,3 +0,0 @@
-export { MastraMCPServerDefinition_alias_1 as MastraMCPServerDefinition } from './_tsup-dts-rollup.cjs';
-export { MastraMCPClient_alias_1 as MastraMCPClient } from './_tsup-dts-rollup.cjs';
-export { MCPConfiguration_alias_1 as MCPConfiguration } from './_tsup-dts-rollup.cjs';
